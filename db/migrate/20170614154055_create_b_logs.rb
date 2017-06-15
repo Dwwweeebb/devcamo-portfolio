4c21276fa0588ed44273b8a8cf001f2b5d666735
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 14:37:03 +0000

    Initial commit

---
+++ db/migrate/20170614154055_create_b_logs.rb
@@ -0,0 +1,10 @@
+class CreateBLogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :b_logs do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
