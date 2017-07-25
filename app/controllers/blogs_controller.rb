04100c192ec6db75353975dc9061ba1f4b312ca4
Dwwweeebb <devinglennj@gmail.com>
2017-07-25 15:22:00 +0000

    Implemneted index styles

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
-
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status ]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
@@ -64,6 +63,14 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+  
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
     end
   end
 
