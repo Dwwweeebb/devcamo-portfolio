22cdb57139ea256d7fd63a13f48f48bf8c00366f
Dwwweeebb <devinglennj@gmail.com>
2017-08-03 00:22:42 +0000

    implemented blog wiget for topics

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,9 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     if logged_in?(:site_admin)
     @blogs = Blog.page(params[:page]).per(5)
@@ -14,8 +13,6 @@ class BlogsController < ApplicationController
     @page_title = "My Portfolio Blog"
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
     if logged_in?(:site_admin) || @blog.Published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
@@ -28,17 +25,13 @@ class BlogsController < ApplicationController
     end
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -92,5 +85,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
+    end
+    
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
